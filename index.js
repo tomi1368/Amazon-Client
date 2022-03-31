e9333ebc5c8cd5100f365b8d2837a9529dfe39ee
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-03-31 10:52:10 -0300

    [update: health route and change test]

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override")
 const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 6005
-
+const {version} = require("./package.json")
 
 
 
@@ -24,6 +24,15 @@ app.use(methodOverride())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
+
+
+app.get("/health",(req,res)=>{ //Me sirve para no deployear algo que no funciona con CI, esto me va a servir para hacer un rollback, que es si no responde voy a deshacer los cambios y dejo una version anterior
+    res.send("Okey")
+})
+
+app.get("/version",(req,res)=>{ //muestro que version estoy deployeando
+    res.send(version)
+})
 
 app.use("/api/",testingRouter)
 app.use("/api/auth",authRouter)
