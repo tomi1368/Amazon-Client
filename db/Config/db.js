1d2a9f0a5e087897e98063da992105a0130e10ae
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-03-31 11:35:59 -0300

    change

--- db/Config/db.js
+++ db/Config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async ()=>{
     console.log(process.env.NODE_ENV=="test",process.env.MONGO_URI_TEST)
-    await mongoose.connect(process.env.NODE_ENV=="test" ? process.env.MONGO_URI_TEST : process.env.MONGO_URI ,{
+    await mongoose.connect( process.env.MONGO_URI ,{
         useNewUrlParser:true,
         useUnifiedTopology:true,
     })
